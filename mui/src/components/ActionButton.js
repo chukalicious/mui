6e178e74b36109340f981912bafb0380f59fef5b
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-19 19:10:40 -0700

    added styles to the main theme

--- mui/src/components/ActionButton.js
+++ mui/src/components/ActionButton.js
@@ -2,7 +2,12 @@ import Button from "@mui/material/Button";
 
 const ActionButton = () => {
   return (
-    <Button color="primary" style={{ marginTop: "1rem" }} variant="outlined">
+    <Button
+      sx={{ fontFamily: "'Fraunces', serif" }}
+      color="secondary"
+      style={{ marginTop: "1rem" }}
+      variant="contained"
+    >
       Click my clit!
     </Button>
   );
--- mui/src/styles/MainTheme.js
+++ mui/src/styles/MainTheme.js
@@ -1,10 +1,29 @@
-import { green } from "@mui/material/colors";
+import { deepOrange, lightBlue, yellow } from "@mui/material/colors";
 import { createTheme } from "@mui/material/styles";
 
 export const mainTheme = createTheme({
   palette: {
     primary: {
-      main: green[500],
+      main: lightBlue.A200,
+      light: lightBlue.A100,
+      dark: lightBlue.A700,
     },
+    secondary: {
+      main: yellow[500],
+      light: yellow[200],
+      dark: "#c8b900",
+    },
+    warning: {
+      main: deepOrange[300],
+      light: deepOrange[100],
+      dark: deepOrange.A400,
+    },
+  },
+  typography: {
+    fontFamily: "'Fraunces', serif, 'Barlow', sans-serif",
+    fontSize: 18,
+    fontWeightLight: 600,
+    fontWeightMedium: 700,
+    fontWeightBold: 900,
   },
 });
