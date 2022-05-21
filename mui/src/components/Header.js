e06a65f8463496691e849a8a505e50c434a2ce41
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-20 23:07:07 -0700

    applied styles to Header.js

--- mui/src/components/Header.js
+++ mui/src/components/Header.js
@@ -2,19 +2,18 @@ import ImageListItem from "@mui/material/ImageListItem";
 import ImageList from "@mui/material/ImageList";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { h1Class } from "../styles/HeaderStyles";
-import { imageListItem, imgList } from "../styles/LandingStyles";
+import { h1Class, imgList, imageListItem } from "../styles/HeaderStyles";
 import imageHeader from "../images/mobile/imageHeader.jpg";
 
 const Header = () => {
   return (
     <Box>
-      <Typography sx={{ ...h1Class }} variant="h1">
-        We are creatives
-      </Typography>
       <ImageList sx={{ ...imgList }}>
+        <Typography sx={{ ...h1Class }} variant="h1">
+          We are creatives
+        </Typography>
         <ImageListItem sx={{ ...imageListItem }} cols={2}>
-          <img src={imageHeader} alt="egg" />
+          <img src={imageHeader} alt="half orange" />
         </ImageListItem>
       </ImageList>
     </Box>
