e061b890b709a92c2f8ca7cfc63ee23d1986567d
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-20 22:36:36 -0700

    added image to header component and did some alignment

--- mui/src/components/Header.js
+++ mui/src/components/Header.js
@@ -1,7 +1,10 @@
+import ImageListItem from "@mui/material/ImageListItem";
 import ImageList from "@mui/material/ImageList";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { h1Class } from "../styles/HeaderStyles";
+import { imageListItem, imgList } from "../styles/LandingStyles";
+import imageHeader from "../images/mobile/imageHeader.jpg";
 
 const Header = () => {
   return (
@@ -9,6 +12,11 @@ const Header = () => {
       <Typography sx={{ ...h1Class }} variant="h1">
         We are creatives
       </Typography>
+      <ImageList sx={{ ...imgList }}>
+        <ImageListItem sx={{ ...imageListItem }} cols={2}>
+          <img src={imageHeader} alt="egg" />
+        </ImageListItem>
+      </ImageList>
     </Box>
   );
 };
