090e5d287ac2a216530d562fab1461d2508ea503
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-20 22:05:32 -0700

    added spacing classes for the image and paragraph containers. styled the h2s, the ps and the links

--- mui/src/components/Landing.js
+++ mui/src/components/Landing.js
@@ -11,7 +11,15 @@ import imageCone from "../images/imageCone.jpg";
 import imageTransform from "../images/mobile/imageTransform.jpg";
 
 // Style Imports
-import { landingLayout, paperStyles, paperBox } from "../styles/LandingStyles";
+import {
+  landingLayout,
+  paperStyles,
+  paperBox,
+  imgTextWrapper,
+  imgList,
+  imageListItem,
+  pLink,
+} from "../styles/LandingStyles";
 
 //Component Imports
 import ActionButton from "./ActionButton";
@@ -20,51 +28,53 @@ const Landing = () => {
   return (
     <Box>
       <Box sx={{ mb: -1 }}>
-        <Box
-          sx={{
-            // border: "purple solid 3px",
-            display: "flex",
-            flexDirection: "column",
-            marginBottom: "-3.2rem",
-          }}
-        >
+        <Box sx={{ ...imgTextWrapper }}>
           <Box sx={{ ...landingLayout }}>
-            <ImageList sx={{ p: 0, mb: "-.5rem", mt: 0 }}>
-              <ImageListItem
-                sx={{
-                  // border: "green solid 3px",
-                  m: 0,
-                }}
-                cols={2}
-              >
+            <ImageList sx={{ ...imgList }}>
+              <ImageListItem sx={{ ...imageListItem }} cols={2}>
                 <img src={imageTransform} alt="egg" />
               </ImageListItem>
             </ImageList>
           </Box>
           <Box sx={{ ...paperBox }}>
             <Paper sx={{ ...landingLayout, ...paperStyles }} elevation={0}>
-              <Typography variant="h2" sx={{ textAlign: "center", m: 1 }}>
-                Transform your brand
-              </Typography>
+              <Typography variant="h2">Transform your brand</Typography>
               <Typography variant="body1" sx={{ textAlign: "center", m: 1 }}>
-                We area full-service creative agency specializing in helping
+                We are a full-service creative agency specializing in helping
                 brands grow fast. Engage your clients through compelling visuals
                 that do most of the marketing for you.
-                <Typography sx={{ fontFamily: "Fraunces" }}>
-                  {" "}
-                  <Link color="primary">Learn More</Link>
+                <Typography sx={{ ...pLink }}>
+                  <Link>Learn More</Link>
                 </Typography>
               </Typography>
             </Paper>
           </Box>
         </Box>
       </Box>
-      <Box sx={{ ...landingLayout }}>
-        <ImageList>
-          <ImageListItem cols={2}>
-            <img src={imageCone} alt="icecream cone" />
-          </ImageListItem>
-        </ImageList>
+      <Box sx={{ ...imgTextWrapper }}>
+        <Box sx={{ ...landingLayout }}>
+          <ImageList sx={{ ...imgList }}>
+            <ImageListItem cols={2}>
+              <img src={imageCone} alt="icecream cone" />
+            </ImageListItem>
+          </ImageList>
+        </Box>
+        <Box sx={{ ...paperBox }}>
+          <Paper sx={{ ...landingLayout, ...paperStyles }} elevation={0}>
+            <Typography variant="h2">
+              Stand out to the right audience
+            </Typography>
+            <Typography variant="body1" sx={{ textAlign: "center", m: 1 }}>
+              Using a collaborative formula of designers, researchers,
+              photographers, videographers, and copywriters, we'll buid and
+              extend your brand in digital places.
+              <Typography sx={{ ...pLink }}>
+                {" "}
+                <Link>Learn More</Link>
+              </Typography>
+            </Typography>
+          </Paper>
+        </Box>
       </Box>
       <Box
         style={{
