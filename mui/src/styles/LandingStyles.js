006caea231df20462d67f472d19649d9ba5fce41
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-20 22:06:14 -0700

    added spacing classes for the image and paragraph containers. styled the h2s, the ps and the links

--- mui/src/styles/LandingStyles.js
+++ mui/src/styles/LandingStyles.js
@@ -3,22 +3,60 @@ export const paperStyles = {
   pb: 6,
   pl: 3,
   pr: 3,
-  // mt: 0,
   borderRadius: 0,
+
+  p: {
+    color: "#7e7c83",
+    mb: 2,
+  },
+
+  h2: {
+    fontSize: "2.5rem",
+    wrap: "wrap",
+    pt: "1.5rem",
+    pb: "1.5rem",
+    color: "#27343e",
+    textAlign: "center",
+    m: 1,
+  },
 };
 
 export const landingLayout = {
   width: "100%",
-  // border: "blue dashed 3px",
 };
 
 export const paperBox = {
   width: "100%",
   mb: 6,
-  // 16px for mb
-  // border: "red 3px solid",
   mt: 1,
   display: "flex",
   flexiDirection: "column",
   minHeight: "50vh",
+};
+
+export const imgTextWrapper = {
+  display: "flex",
+  flexDirection: "column",
+  marginBottom: "-3.2rem",
+};
+
+export const imgList = {
+  p: 0,
+  mb: "-.5rem",
+  mt: 0,
+};
+
+export const imageListItem = {
+  m: 0,
+};
+
+export const pLink = {
+  pt: 2,
+  fontFamily: "Fraunces",
+  textTransform: "uppercase",
+  letterSpacing: ".15rem",
+  a: {
+    color: "#27343e",
+    textDecorationColor: "secondary",
+  },
 };
