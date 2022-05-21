b4170163ef9bebcf176a6cc8bda01be500bee277
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-20 23:19:23 -0700

    adjusted primary.main color to match it more closely to the half orange image

--- mui/src/styles/MainTheme.js
+++ mui/src/styles/MainTheme.js
@@ -4,7 +4,7 @@ import { createTheme } from "@mui/material/styles";
 export const mainTheme = createTheme({
   palette: {
     primary: {
-      main: "#53ccfd",
+      main: "#3dbefe",
       light: lightBlue.A100,
       dark: lightBlue.A700,
     },
