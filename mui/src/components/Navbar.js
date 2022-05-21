57f43ba9f4ac0eb853ddad68d3df460fad8b24d3
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-20 23:29:50 -0700

    created navbar styles directory and created and applied styles for the toolbar

--- mui/src/components/Navbar.js
+++ mui/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import { toolbarStyles } from "../styles/NavbarStyles";
 
 import Title from "./Title";
 import HamburgerMenu from "./HamburgerMenu";
@@ -9,7 +10,7 @@ const Navbar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar elevation={0} position="static">
-        <Toolbar style={{ color: "white" }}>
+        <Toolbar sx={{ ...toolbarStyles }}>
           <Title />
           <HamburgerMenu />
         </Toolbar>
---
+++ mui/src/styles/NavbarStyles.js
@@ -0,0 +1,5 @@
+export const toolbarStyles = {
+  border: "#3dbefe solid .01px",
+  p: 2,
+  color: "white",
+};
