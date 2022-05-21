bd3972d20a7532dc292136cf6fc61744220dffa0
Katiuska Alicea <katiuskaalicea@gmail.com>
2022-05-20 23:06:36 -0700

    created styles for the headers image and h1, took them mostly from already created styles for the landing page

--- mui/src/styles/HeaderStyles.js
+++ mui/src/styles/HeaderStyles.js
@@ -1,10 +1,25 @@
 export const h1Class = {
   color: "#f0f0f0",
+  zIndex: 1100,
   textTransform: "uppercase",
   textAlign: "center",
   letterSpacing: ".28rem",
   mt: 5,
   mb: 5,
+};
+
+export const imgList = {
+  p: 0,
+  mb: "-.5rem",
+  mt: 0,
+  alignContent: "center",
+  display: "flex",
+  flexDirection: "column",
+};
+
+export const imageListItem = {
+  m: 0,
+  mt: -45,
 };
 
 // style for Box with centered content
